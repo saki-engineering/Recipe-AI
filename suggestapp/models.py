8da2f594698fbfde7cad53b99d1bb63022e2ba24
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-01-16 17:34:38 +0900

    レシピのmodelフィールドの作成・マイグレーション

--- suggestapp/models.py
+++ suggestapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+"""
+Recipe: オレンジページ掲載のレシピ情報
+
+    title: レシピ名
+    url_id: URLのdetail部分のID ex)https://www.orangepage.net/recipes/detail_XXXXXX のXXXXXX部分
+    step: 調理工程数
+    calory: カロリー
+    salt: 塩分量
+"""
+class Recipe(models.Model):
+    title  = models.CharField(blank=False, null=False, max_length=100)
+    url_id = models.PositiveIntegerField(blank=False, null=False)
+    step   = models.SmallIntegerField(blank=False, null=False)
+    calory = models.DecimalField(blank=False, null=False, max_digits=6, decimal_places=2)
+    salt   = models.DecimalField(blank=False, null=False, max_digits=4, decimal_places=2)
