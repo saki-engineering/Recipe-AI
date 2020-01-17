416e7391517fe578cda52894b9c523a7cbbe55e5
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-01-18 01:32:47 +0900

    レシピ選定アルゴリズムを実装

--- suggestapp/views.py
+++ suggestapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
+import numpy as np
+import random
+import math
 import requests
 from bs4 import BeautifulSoup as bs
 from .models import Recipe
@@ -9,13 +12,32 @@ def index(request):
     return render(request, 'suggestapp/index.html')
 
 def search(request, param):
-    recipes = Recipe.objects.all()
+    #paramをnumpy配列に変換する
+    user_ans = np.zeros(10)
+    for i in reversed(range(10)):
+        user_ans[i] = param % 10
+        param = (param - user_ans[i])//10
 
-    page_url = "https://www.orangepage.net/recipes/detail_" + str(recipes[0].url_id)
+    NORMALIZE_STEP = 0.2
+    NORMALIZE_CALORY = 0.2
+    NORMALIZE_SALT = 0.2
+
+    user_vector = np.array([user_ans[1] * NORMALIZE_STEP, user_ans[2] * NORMALIZE_CALORY, user_ans[3] * NORMALIZE_SALT], dtype=float)
+    recipes = Recipe.objects.all()
+    candicates = []
+    thres = 0
+    for recipe in recipes:
+        menu_vector = np.array([recipe.step / 10, recipe.calory / 400, recipe.salt / 4], dtype=float)
+        if np.dot(user_vector, menu_vector) >= thres:
+            candicates.append(recipe)
+
+    index = math.ceil(random.random() * len(candicates))
+
+    #画面に表示する料理画像のurlを取得
+    page_url = "https://www.orangepage.net/recipes/detail_" + str(recipes[index].url_id)
     res = requests.get(page_url, allow_redirects=False)
     bsObj = bs(res.text, "html.parser")
-
     image_url = bsObj.find("img", itemprop="image").get("src")
 
-    ret = {"title": recipes[0].title, "url_id": recipes[0].url_id, "image_url": image_url}
+    ret = {"title": recipes[index].title, "url_id": recipes[index].url_id, "image_url": image_url}
     return JsonResponse(ret)
